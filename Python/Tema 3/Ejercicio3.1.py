1573c56034c958425ba7bf48841864cd80e6e7d2
Gabriela Patiño <100980917+Gabyp05@users.noreply.github.com>
2022-07-12 19:02:25 -0400

    Add files via upload

---
+++ Python/Tema 3/Ejercicio3.1.py
@@ -0,0 +1,21 @@
+nombre: str = "Gabriela"
+apellidos: str = "Patiño"
+edad: int = 24
+email: str = "gabyapd05@gmail.com"
+tlf: int = 4142931360
+casada: bool = 'false'
+hijos: bool ='false'
+amigos: list = ['Betsi','Eliana','Aranza']
+peliculasVistas = {"Clave1": "Ya te extraño",
+                       "Clave2":"Cenicienta",
+                       "Clave3": "Guardianes de la galaxia"}
+
+print(nombre)
+print(apellidos)
+print(edad)
+print(email)
+print(tlf)
+print(casada)
+print(hijos)
+print(amigos)
+print(peliculasVistas)
