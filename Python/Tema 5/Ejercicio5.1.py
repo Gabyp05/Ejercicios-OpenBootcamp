40cbf0f39e2fce50e7d1d10e96b491e244ac6bc5
Gabriela Patiño <100980917+Gabyp05@users.noreply.github.com>
2022-07-14 11:27:13 -0400

    Add files via upload

---
+++ Python/Tema 5/Ejercicio5.1.py
@@ -0,0 +1,11 @@
+import math
+
+def areaTriangulo (altura, base):
+    return ((altura*base/2))
+
+print(f"El area del triangulo es: {areaTriangulo(4,3)}" )
+
+def areaCirculo (radio):
+    return (math.pi*(radio**2))
+
+print (f"El area del circulo es: {areaCirculo(6)}")
