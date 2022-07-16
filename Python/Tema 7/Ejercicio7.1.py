99ecba310dde427e6daaa0c0a67e898eb21178ff
Gabriela Patiño <100980917+Gabyp05@users.noreply.github.com>
2022-07-16 17:16:32 -0400

    Add files via upload

---
+++ Python/Tema 7/Ejercicio7.1.py
@@ -0,0 +1,9 @@
+from operadores import * 
+
+num1, num2 = (5, 2)
+
+print("{} + {} = {}".format(num1, num2, suma(num1, num2)))
+print( "{} - {} = {}".format(num1, num2, resta(num1, num2)))
+print( "{} * {} = {}".format(num1, num2, multiplicacion(num1, num2)))
+print( "{} / {} = {}".format(num1, num2, division(num1, num2)))
+    
