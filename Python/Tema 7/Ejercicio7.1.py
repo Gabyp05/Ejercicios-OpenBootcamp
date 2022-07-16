4a14c8eafa34d61511c22db08ba90e17cbc74f83
Gabriela Patiño <100980917+Gabyp05@users.noreply.github.com>
2022-07-16 17:27:17 -0400

    Delete Python/Tema 7 directory

--- Python/Tema 7/Ejercicio7.1.py
+++
@@ -1,9 +0,0 @@
-from operadores import * 
-
-num1, num2 = (5, 2)
-
-print("{} + {} = {}".format(num1, num2, suma(num1, num2)))
-print( "{} - {} = {}".format(num1, num2, resta(num1, num2)))
-print( "{} * {} = {}".format(num1, num2, multiplicacion(num1, num2)))
-print( "{} / {} = {}".format(num1, num2, division(num1, num2)))
-    
